7cb0dacc4884b73c5f5b57216d147bc9a0519a8b
Md. Ashikur Rahman <ashikur.rahman@bjitacademy.com>
2023-02-09 07:59:14 +0600

    Initial Commit

---
+++ Cricket_Info/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cricket_Info
+//
+//  Created by bjit on 9/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
